ce6d1437bccfde80afe1b01569a056ddab42bba0
Adithya Kiran <36990588+adi-kiran@users.noreply.github.com>
2023-10-14 06:01:23 +0530

    Add Graphs and Update Calorie Page Cards (#18)

--- frontend/src/components/UserCaloriesPage.js
+++ frontend/src/components/UserCaloriesPage.js
@@ -5,6 +5,8 @@ import {
   CardContent,
   CardHeader,
   Container,
+  ListItem,
+  List,
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -21,26 +23,32 @@ import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
 import StarIcon from "@mui/icons-material/Star";
 import TimelineIcon from "@mui/icons-material/Timeline";
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
+import LocalCafeIcon from "@mui/icons-material/LocalCafe";
+import TodayIcon from "@mui/icons-material/Today";
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  ResponsiveContainer,
+  Cell,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+} from "recharts";
 
 function UserCaloriesPage(props) {
-  const data01 = [
-    {
-      name: "Consumed",
-      value: 400,
-    },
-  ];
-  const data02 = [
-    {
-      name: "Burned",
-      value: 250,
-    },
-    {
-      name: "Remaining to Goal",
-      value: 150,
-    },
-  ];
+  const [todayCaloriesConsumed, setTodayCaloriesConsumed] = useState(500);
+  const [todayCaloriesBurned, setTodayCaloriesBurned] = useState(200);
+  const [todayGoal, setTodayGoal] = useState(300);
+  const [events, setEvents] = useState([]);
+  const COLORS = ["#8b0e0e", "#97a3a2"];
   const [foodItems, setFoodItems] = useState({});
+  const [dietHistory, setDietHistory] = useState([]);
+  const [weekHistory, setWeekHistory] = useState([]);
 
   useEffect(() => {
     // Make API call to backend to get food items and their calories from DB.
@@ -48,6 +56,279 @@ function UserCaloriesPage(props) {
     let data = { Potato: 50, Acai: 20, Cheeseburger: 80 };
     // set the foodItems variable with the key-value data
     setFoodItems(data);
+    // TO DO: UPDATE THIS API CALL TO PERFORM THE ABOVE FUNCTIONALITY AND REMOVE THE ABOVE LINES
+    // axios({
+    //   method: "GET",
+    //   url: "/GET_FOOD_ITEMS",
+    //   headers: {
+    //     Authorization: "Bearer " + props.token,
+    //   },
+    // })
+    //   .then((response) => {
+    //     const res = response.data;
+    //     setFoodItems(res);
+    //   })
+    //   .catch((error) => {
+    //     if (error.response) {
+    //       console.log(error.response);
+    //       console.log(error.response.status);
+    //       console.log(error.response.headers);
+    //     }
+    //   });
+
+    // Make API call to backend to get last 7 days history from DB.
+    // Either ensure API sends in the below format, or format in theis method on receiving it, to ensure it is in the below format
+    let dietHistoryAPIResp = [
+      {
+        dayIndex: 0,
+        date: "10/13/2023",
+        foodConsumed: [
+          {
+            item: "Chicken Salad",
+            calories: 500,
+          },
+          {
+            item: "Onion Soup",
+            calories: 300,
+          },
+          {
+            item: "Potato Salad",
+            calories: 500,
+          },
+          {
+            item: "Cheese Burger",
+            calories: 500,
+          },
+        ],
+        caloriesConsumed: 1800,
+        exceededDailyLimit: false,
+        burntCalories: 1200,
+      },
+      {
+        dayIndex: 1,
+        date: "10/12/2023",
+        foodConsumed: [
+          {
+            item: "Chicken Salad",
+            calories: 500,
+          },
+          {
+            item: "Onion Soup",
+            calories: 300,
+          },
+          {
+            item: "Potato Salad",
+            calories: 500,
+          },
+          {
+            item: "Cheese Burger",
+            calories: 500,
+          },
+        ],
+        caloriesConsumed: 1800,
+        exceededDailyLimit: true,
+        burntCalories: 1000,
+      },
+      {
+        dayIndex: 2,
+        date: "10/11/2023",
+        foodConsumed: [
+          {
+            item: "Chicken Salad",
+            calories: 500,
+          },
+          {
+            item: "Onion Soup",
+            calories: 300,
+          },
+          {
+            item: "Potato Salad",
+            calories: 500,
+          },
+          {
+            item: "Cheese Burger",
+            calories: 500,
+          },
+        ],
+        caloriesConsumed: 1800,
+        exceededDailyLimit: false,
+        burntCalories: 900,
+      },
+      {
+        dayIndex: 3,
+        date: "10/10/2023",
+        foodConsumed: [
+          {
+            item: "Chicken Salad",
+            calories: 500,
+          },
+          {
+            item: "Onion Soup",
+            calories: 300,
+          },
+          {
+            item: "Potato Salad",
+            calories: 500,
+          },
+          {
+            item: "Cheese Burger",
+            calories: 500,
+          },
+        ],
+        caloriesConsumed: 1800,
+        exceededDailyLimit: true,
+        burntCalories: 400,
+      },
+      {
+        dayIndex: 4,
+        date: "10/9/2023",
+        foodConsumed: [
+          {
+            item: "Chicken Salad",
+            calories: 500,
+          },
+          {
+            item: "Onion Soup",
+            calories: 300,
+          },
+          {
+            item: "Potato Salad",
+            calories: 500,
+          },
+          {
+            item: "Cheese Burger",
+            calories: 500,
+          },
+        ],
+        caloriesConsumed: 1800,
+        exceededDailyLimit: false,
+        burntCalories: 500,
+      },
+      {
+        dayIndex: 5,
+        date: "10/8/2023",
+        foodConsumed: [
+          {
+            item: "Chicken Salad",
+            calories: 500,
+          },
+          {
+            item: "Onion Soup",
+            calories: 300,
+          },
+          {
+            item: "Potato Salad",
+            calories: 500,
+          },
+          {
+            item: "Cheese Burger",
+            calories: 500,
+          },
+        ],
+        caloriesConsumed: 1800,
+        exceededDailyLimit: false,
+        burntCalories: 500,
+      },
+      {
+        dayIndex: 6,
+        date: "10/7/2023",
+        foodConsumed: [
+          {
+            item: "Chicken Salad",
+            calories: 500,
+          },
+          {
+            item: "Onion Soup",
+            calories: 300,
+          },
+          {
+            item: "Potato Salad",
+            calories: 500,
+          },
+          {
+            item: "Cheese Burger",
+            calories: 500,
+          },
+        ],
+        caloriesConsumed: 1800,
+        exceededDailyLimit: true,
+        burntCalories: 700,
+      },
+    ];
+    // set the dietHistory variable with the key-value data
+    setDietHistory(dietHistoryAPIResp);
+    // Generate weekHistoryData from the received dietHistoryAPIResp using the below code
+    let weekHistoryData = dietHistoryAPIResp.map((dayObj) => {
+      return {
+        date: dayObj.date,
+        consumedCalories: dayObj.caloriesConsumed,
+        burntCalories: dayObj.burntCalories,
+      };
+    });
+    // set the weekHistory variable with the generated data
+    setWeekHistory(weekHistoryData);
+
+    // TO DO: UPDATE THIS API CALL TO PERFORM THE ABOVE FUNCTIONALITY AND REMOVE THE ABOVE LINES
+    // axios({
+    //   method: "GET",
+    //   url: "/GET_DIET_HISTORY",
+    //   headers: {
+    //     Authorization: "Bearer " + props.token,
+    //   },
+    // })
+    //   .then((response) => {
+    //     const res = response.data;
+    //     setDietHistory(res);
+    //     let weekHistoryData = dietHistoryAPIResp.map((dayObj) => {
+    //       return {
+    //         date: dayObj.date,
+    //         consumedCalories: dayObj.caloriesConsumed,
+    //         burntCalories: dayObj.burntCalories,
+    //       };
+    //     });
+    //     setWeekHistory(weekHistoryData);
+    //   })
+    //   .catch((error) => {
+    //     if (error.response) {
+    //       console.log(error.response);
+    //       console.log(error.response.status);
+    //       console.log(error.response.headers);
+    //     }
+    //   });
+    // Make API call to backend to get food items and their calories from DB.
+    // Either ensure API sends in the below format, or format in theis method on receiving it, to ensure it is in the below format
+
+    let eventsData = [
+      {
+        eventName: "Yoga",
+        date: "10/19/2023",
+      },
+      {
+        eventName: "Swimming",
+        date: "10/20/2023",
+      },
+    ];
+    // set the foodItems variable with the key-value data
+    setEvents(eventsData);
+    // TO DO: UPDATE THIS API CALL TO PERFORM THE ABOVE FUNCTIONALITY AND REMOVE THE ABOVE LINES
+    // axios({
+    //   method: "GET",
+    //   url: "/GET_USERS_EVENTS",
+    //   headers: {
+    //     Authorization: "Bearer " + props.token,
+    //   },
+    // })
+    //   .then((response) => {
+    //     const res = response.data;
+    //     setEvents(res);
+    //   })
+    //   .catch((error) => {
+    //     if (error.response) {
+    //       console.log(error.response);
+    //       console.log(error.response.status);
+    //       console.log(error.response.headers);
+    //     }
+    //   });
   }, []);
 
   const [intakeItem, setIntakeItem] = useState("");
@@ -57,10 +338,6 @@ function UserCaloriesPage(props) {
     setIntakeCalories(foodItems[event.target.value]);
   };
   const [intakeDate, setIntakeDate] = useState(dayjs());
-
-  const [burntoutCalories, setBurntoutCalories] = useState("");
-  const [burnoutDate, setBurnoutDate] = useState(dayjs());
-
   const handleAddCalorieIntake = (e) => {
     console.log("Intake", intakeItem, intakeCalories, intakeDate);
     // TO DO: UPDATE THE API CALL
@@ -91,6 +368,8 @@ function UserCaloriesPage(props) {
     //   });
   };
 
+  const [burntoutCalories, setBurntoutCalories] = useState("");
+  const [burnoutDate, setBurnoutDate] = useState(dayjs());
   const handleAddCalorieBurnout = () => {
     console.log("Burnout", burntoutCalories, burnoutDate);
     // TO DO: UPDATE THE API CALL
@@ -130,7 +409,9 @@ function UserCaloriesPage(props) {
             gap: 2,
             gridTemplateRows: "auto",
             gridTemplateAreas: `"today today exercise exercise intake intake intake"
-                              "week week week week burntout burntout burntout"`,
+                              "week week week week burntout burntout burntout"
+                              "week week week week events events events"
+                              "hist hist hist hist hist hist hist"`,
             paddingTop: "2rem",
           }}
         >
@@ -141,49 +422,61 @@ function UserCaloriesPage(props) {
               avatar={<StarIcon />}
             />
             <CardContent>
-              <PieChart width={350} height={160}>
+              {/* <ResponsiveContainer width="100%" height="100%"> */}
+              <PieChart width={375} height={160}>
                 <Pie
-                  data={data02}
+                  data={[
+                    { name: "Calories Burned", value: todayCaloriesBurned },
+                    {
+                      name: "Calories to goal",
+                      value: todayGoal - todayCaloriesBurned,
+                    },
+                  ]}
                   dataKey="value"
                   nameKey="name"
                   cx="50%"
                   cy="50%"
-                  outerRadius={50}
-                  innerRadius={30}
-                  fill="#8884d8"
-                />
-                <Pie
-                  data={data01}
-                  dataKey="value"
-                  nameKey="name"
-                  cx="50%"
-                  cy="50%"
-                  innerRadius={60}
                   outerRadius={80}
-                  fill="#82ca9d"
-                  label
+                  innerRadius={60}
+                  fill="#8b0e0e"
+                >
+                  {COLORS.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+                <Pie
+                  data={[
+                    { name: "Calories Consumed", value: todayCaloriesConsumed },
+                  ]}
+                  dataKey="value"
+                  nameKey="name"
+                  cx="50%"
+                  cy="50%"
+                  innerRadius={30}
+                  outerRadius={50}
+                  fill="#19229e"
                 />
+                <Tooltip />
+                <Legend layout="vertical" verticalAlign="top" align="right" />
               </PieChart>
+              {/* </ResponsiveContainer> */}
             </CardContent>
           </Card>
           <Card sx={{ gridArea: "exercise" }} elevation={5}>
             <CardHeader
               title={"Exercise of the Day"}
               subheader={"Today's pick to help you get you fit"}
-              avatar={
-                <>
-                  <SportsMartialArtsIcon />
-                  <DirectionsRunIcon />
-                  <FitnessCenterIcon />
-                </>
-              }
+              avatar={<FitnessCenterIcon />}
             />
           </Card>
           <Card sx={{ gridArea: "intake" }} elevation={5}>
             <CardHeader
               title={"Calorie Intake"}
               subheader={"Enter the food and calories consumed to track it"}
-              avatar = {<FastfoodIcon/>}
+              avatar={<FastfoodIcon />}
             />
             <CardContent>
               <form onSubmit={handleAddCalorieIntake}>
@@ -244,12 +537,72 @@ function UserCaloriesPage(props) {
               </form>
             </CardContent>
           </Card>
+          <Card sx={{ gridArea: "events" }} elevation={5}>
+            <CardHeader
+              title={"Upcoming Events"}
+              subheader={"These are the upcoming events you are enrolled in"}
+              avatar={
+                <>
+                  <SportsMartialArtsIcon />
+                  <DirectionsRunIcon />
+                </>
+              }
+            />
+            <CardContent>
+              <List>
+                {events.map((eventObj, ind) => {
+                  return (
+                    <ListItem
+                      key={`item-${ind}`}
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-around",
+                      }}
+                    >
+                      <div>{eventObj.eventName}</div>
+                      <div>{eventObj.date}</div>
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </CardContent>
+          </Card>
           <Card sx={{ gridArea: "week" }} elevation={5}>
             <CardHeader
               title={"Weekly Stats"}
               subheader={"Track your performance over the last week"}
               avatar={<TimelineIcon />}
             />
+            <CardContent>
+              <LineChart
+                width={800}
+                height={300}
+                data={weekHistory}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="consumedCalories"
+                  stroke="#19229e"
+                />
+                <Line
+                  type="monotone"
+                  dataKey="burntCalories"
+                  stroke="#8b0e0e"
+                  activeDot={{ r: 4 }}
+                />
+              </LineChart>
+            </CardContent>
           </Card>
           <Card sx={{ gridArea: "burntout" }} elevation={5}>
             <CardHeader
@@ -282,6 +635,67 @@ function UserCaloriesPage(props) {
                   </Button>
                 </Box>
               </form>
+            </CardContent>
+          </Card>
+          <Card sx={{ gridArea: "hist" }} elevation={5}>
+            <CardHeader
+              title={"Diet Tracker"}
+              subheader={"This Week's Calories Consumed"}
+              avatar={
+                <>
+                  <LunchDiningIcon />
+                  <LocalCafeIcon />
+                </>
+              }
+            />
+            <CardContent
+              sx={{
+                display: "grid",
+                gridTemplateColumns: "repeat(7, 1fr)",
+                gap: 2,
+                gridTemplateRows: "auto",
+                gridTemplateAreas: `"day-0 day-1 day-2 day-3 day-4 day-5 day-6"`,
+                paddingTop: "2rem",
+              }}
+            >
+              {dietHistory.map((day, index) => {
+                return (
+                  <Card sx={{ gridArea: `day-${day.dayIndex}` }} elevation={5}>
+                    <CardHeader title={day.date} avatar={<TodayIcon />} />
+                    <CardContent>
+                      <div
+                        style={{
+                          color: day.exceededDailyLimit ? "red" : "green",
+                          textAlign: "center",
+                          fontWeight: "bold",
+                          paddingBottom: "10px",
+                        }}
+                      >{`Total Calories : ${day.caloriesConsumed}`}</div>
+                      <div style={{ textAlign: "center", fontWeight: "bold" }}>
+                        Food Consumed
+                      </div>
+                      <div>
+                        <List>
+                          {day.foodConsumed.map((itemObj, ind) => {
+                            return (
+                              <ListItem
+                                key={`item-${ind}`}
+                                sx={{
+                                  display: "flex",
+                                  justifyContent: "space-between",
+                                }}
+                              >
+                                <div>{itemObj.item}</div>
+                                <div>{itemObj.calories}</div>
+                              </ListItem>
+                            );
+                          })}
+                        </List>
+                      </div>
+                    </CardContent>
+                  </Card>
+                );
+              })}
             </CardContent>
           </Card>
         </Box>
