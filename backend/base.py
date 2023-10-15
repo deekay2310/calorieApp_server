59c46defc9bb9467d2fb9a3de34ed8cf47bbbcb2
Adithya Kiran <adithyakiran1999@gmail.com>
2023-10-15 04:53:28 -0400

    Add 4 APIs for User Calorie Page

--- backend/base.py
+++ backend/base.py
@@ -6,6 +6,8 @@ from flask_pymongo import PyMongo
 from datetime import datetime, timedelta, timezone
 from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
                                unset_jwt_cookies, jwt_required, JWTManager
+from datetime import datetime, timedelta
+from functools import reduce
 
 
 api = Flask(__name__)
@@ -28,7 +30,7 @@ def create_token():
         return jsonify({"message": "Login successful", "access_token":access_token})
     else:
         print("Invalid email or password")
-        return jsonify({"message": "Invalid email or password"}, 401)
+        return jsonify({"message": "Invalid email or password"}),401
 
 @api.route("/register", methods=['POST'])
 def register():
@@ -131,3 +133,121 @@ def my_profile():
     }
 
     return response_body
+
+@api.route('/caloriesConsumed',methods=["POST"])
+@jwt_required()
+def addUserConsumedCalories():
+    print("Hello")
+    data = request.get_json()  # get data from POST request
+    current_user = get_jwt_identity()
+    print(data)
+    print(current_user)
+    try:
+        # Insert data into MongoDB
+        mongo.db.user.update_one({'email': current_user, "consumedDate": data['intakeDate']}, {"$push": {"foodConsumed": {"item":data["intakeFoodItem"],"calories":data["intakeCalories"]}}}, upsert=True)
+        response = {"status": "Data saved successfully"}
+        statusCode = 200
+    except Exception as e:
+        response = {"status": "Error", "message": str(e)}
+        statusCode = 500
+    return jsonify(response),statusCode
+
+@api.route('/caloriesBurned',methods=["POST"])
+@jwt_required()
+def addUserBurnedCalories():
+    print("Hello")
+    data = request.get_json()  # get data from POST request
+    current_user = get_jwt_identity()
+    print(data)
+    print(current_user)
+    try:
+        # Insert data into MongoDB
+        mongo.db.user.update_one({'email': current_user, "consumedDate": data['burnoutDate']}, {"$inc": {"burntCalories": int(data["burntoutCalories"])}}, upsert=True)
+        response = {"status": "Data saved successfully"}
+        statusCode = 200
+    except Exception as e:
+        response = {"status": "Error", "message": str(e)}
+        statusCode = 500
+    return jsonify(response),statusCode
+
+@api.route('/weekHistory',methods=["POST"])
+@jwt_required()
+def getWeekHistory():
+    data = request.get_json()  # get data from POST request
+    print(data)
+    current_user = get_jwt_identity()
+    todayDate = datetime.strptime(data["todayDate"],"%m/%d/%Y")
+    dates = [(todayDate-timedelta(days=x)).strftime("%m/%d/%Y") for x in range(7)]
+    print(dates)
+    print(current_user)
+    calorieLimit = 1000
+    result = []
+    try:
+        for index,dateToFind in enumerate(dates):
+            # Every day's res item should like this
+            # {
+            #   dayIndex: 0,               #Interger from 0-6
+            #   date: "10/13/2023",        #Date 0=today, 6=7th day ago from today
+            #   foodConsumed: [            # A list of dicts, each dict contains a food item and its calories
+            #     {
+            #       item: "Chicken Salad",
+            #       calories: 500,
+            #     },
+            #     {
+            #       item: "Onion Soup",
+            #       calories: 300,
+            #     },
+            #     {
+            #       item: "Potato Salad",
+            #       calories: 500,
+            #     },
+            #     {
+            #       item: "Cheese Burger",
+            #       calories: 500,
+            #     },
+            #   ],
+            #   caloriesConsumed: 1800,    # the sum of all calories consumed from above list
+            #   exceededDailyLimit: false, # true or false based on whether caloriesConsumed is > limit user set
+            #   burntCalories: 1200,       # calories burnt out on that day
+            # }
+            res = {}
+            data = mongo.db.user.find_one({'email': current_user, "consumedDate": dateToFind})
+            print(data)
+            res["dayIndex"] = index
+            res["date"] = dateToFind
+            if data:
+                if "foodConsumed" in data:
+                    res["foodConsumed"] = data["foodConsumed"]
+                    res["caloriesConsumed"] = reduce(lambda a,b: a+b, [int(item["calories"]) for item in data["foodConsumed"]])
+                    res["exceededDailyLimit"] = res["caloriesConsumed"]>calorieLimit
+                if "burntCalories" in data:
+                    res["burntCalories"] = data["burntCalories"]
+            if "foodConsumed" not in res:
+                res["foodConsumed"] = []
+            if "caloriesConsumed" not in res:
+                res["caloriesConsumed"] = 0
+            if "burntCalories" not in res:
+                res["burntCalories"] = 0
+            if "exceededDailyLimit" not in res:
+                res["exceededDailyLimit"] = False
+            result.append(res)
+        response = result
+        statusCode = 200
+    except Exception as e:
+        response = {"status": "Error", "message": str(e)}
+        statusCode = 500
+    return jsonify(response),statusCode
+
+@api.route('/foodCalorieMapping',methods=["GET"])
+@jwt_required()
+def getFoodCalorieMapping():
+    try:
+        data = mongo.db.food.find()
+        # Response should be in this format {foodItem: calories, foodItem: calories....} 
+        # For Example { Potato: 50, Acai: 20, Cheeseburger: 80 }
+        response = {item["food"]:item["calories"] for item in data}
+        statusCode = 200
+    except Exception as e:
+        response = {"status": "Error", "message": str(e)}
+        statusCode = 500
+    return jsonify(response),statusCode
