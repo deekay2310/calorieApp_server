72d15f90d07246542c0083409b4ca4f26b51be3c
sharonjoji99 <nsjoji@ncsu.edu>
2023-10-15 03:15:49 -0400

    something

--- backend/test_api.py
+++ backend/test_api.py
@@ -8,28 +8,26 @@ class APITestCase(unittest.TestCase):
     def setUp(self):
         self.app = api
         self.app.config['TESTING'] = True
+        self.app.mongo_client = mongomock.MongoClient()  # Use mongomock for testing
         self.client = self.app.test_client()
 
     
     def test_get_events(self):
-        with patch('pymongo.MongoClient') as mock_mongo_client:
-            # Create a mock MongoClient instance
-            mock_mongo = mongomock.MongoClient()
-            mock_mongo_client.return_value = mock_mongo
+        # Create a mock collection
+        db = self.app.mongo_client['test']
+        collection = db['events']
 
-            # Create a mock collection
-            mock_db = mock_mongo['test']
-            mock_collection = mock_db['events']
+        # Replace the collection's find method with a Mock object
+        mock_find = Mock()
+        collection.find = mock_find
+        mock_find.return_value = [
+            {"name": "Event 1"},
+            {"name": "Event 2"},
+        ]
 
-            # Mock the find method on the collection
-            mock_collection.find = Mock()
-            mock_collection.find.return_value = [
-                {"name": "Event 1"},
-                {"name": "Event 2"},
-            ]
+        response = self.client.get('/events')
+        self.assertEqual(response.status_code, 200)
 
-            response = self.client.get('/events')
-            self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
